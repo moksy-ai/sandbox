2d83c391503fec0b638e163de33b2312138efd62
mr-tajul-Islam <tajul.azazil@gmail.com>
2026-01-08 22:52:55 +0600

    changes

---
+++ templates/bootcamp/saturn/ai/assets/scripts/scripts.js
@@ -0,0 +1,38 @@
+{
+           "@context": "http://www.schema.org",
+           "@type": "ProfessionalService",
+           "name": "Saturn",
+           "title": "Saturn - Your Online Learning Solution",
+           "url": "/",
+           "sameAs": [
+              ""
+           ],
+           "logo": "https://moksy.com/api/build/uploads/78037205/uploads/logo.png",
+           "priceRange": "$$$",
+           "image": "https://moksy.com/api/build/uploads/78037205/uploads/socialshare.png",
+           "description": "Saturn is your complete online learning solution. Access a variety of courses and enhance your skills and knowledge.",
+           "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "3-5, Marco Polo House, Lansdowne Road",
+              "addressLocality": "London",
+              "addressRegion": "Surrey",
+              "postalCode": "CR0 2BX",
+              "addressCountry": "United Kingdom"
+           },
+           "email": "support@moksy.com",
+           "geo": {
+              "@type": "GeoCoordinates",
+              "latitude": "51.37683",
+              "longitude": "-0.09728"
+           },
+           
+           "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
+           "telephone": "+44 0808 175 1749",
+           "aggregateRating": {
+                 "@type": "AggregateRating",
+                 "ratingValue": "",
+                 "ratingCount": ""
+               },
+          "datePublished": ""
+         }
+Value for custom_js_code
