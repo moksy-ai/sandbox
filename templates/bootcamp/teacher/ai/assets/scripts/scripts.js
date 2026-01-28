0612d4ce4950a609e60076d86d05ee6fb6ee2ddc
mr-tajul-Islam <tajul.azazil@gmail.com>
2026-01-28 19:30:14 +0600

    changes

---
+++ templates/bootcamp/teacher/ai/assets/scripts/scripts.js
@@ -0,0 +1,38 @@
+{
+           "@context": "http://www.schema.org",
+           "@type": "ProfessionalService",
+           "name": "Teacher",
+           "title": "Teacher - Online Courses for Lifelong Learning",
+           "url": "/",
+           "sameAs": [
+              ""
+           ],
+           "logo": "https://moksy.com/api/build/uploads/88952172/uploads/logo.png",
+           "priceRange": "$$$",
+           "image": "https://moksy.com/api/build/uploads/88952172/uploads/socialshare.png",
+           "description": "Teacher offers online courses for lifelong learning. Discover our diverse range of courses and start your educational journey today.",
+           "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "3-5, Marco Polo House, Lansdowne Road",
+              "addressLocality": "London",
+              "addressRegion": "Surrey",
+              "postalCode": "CR0 2BX",
+              "addressCountry": "United Kingdom"
+           },
+           "email": "support@moksy.com",
+           "geo": {
+              "@type": "GeoCoordinates",
+              "latitude": "51.37683",
+              "longitude": "-0.09728"
+           },
+           
+           "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
+           "telephone": "+44 0808 175 1749",
+           "aggregateRating": {
+                 "@type": "AggregateRating",
+                 "ratingValue": "",
+                 "ratingCount": ""
+               },
+          "datePublished": ""
+         }
+Value for custom_js_code
