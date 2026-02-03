b7c616307b949946b5001d1504323556c07dd1f5
mr-tajul-Islam <tajul.azazil@gmail.com>
2026-02-03 17:17:08 +0600

    changes

---
+++ templates/bootcamp/dice/ai/assets/scripts/scripts.js
@@ -0,0 +1,39 @@
+{
+           "@context": "http://www.schema.org",
+           "@type": "ProfessionalService",
+           "name": "Dice",
+           "title": "Dice - Comprehensive Online Education",
+           "url": "/",
+           "sameAs": [
+              ""
+           ],
+           "logo": "https://moksy.com/api/build/uploads/81854606/uploads/logo.png",
+           "priceRange": "$$$",
+           "image": "https://moksy.com/api/build/uploads/81854606/uploads/socialshare.png",
+           "description": "Dice offers comprehensive online education. Access a wide range of courses and resources to enhance your learning experience.",
+           "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "3-5, Marco Polo House, Lansdowne Road",
+              "addressLocality": "London",
+              "addressRegion": "Surrey",
+              "postalCode": "CR0 2BX",
+              "addressCountry": "United Kingdom"
+           },
+           "email": "support@moksy.com",
+           "geo": {
+              "@type": "GeoCoordinates",
+              "latitude": "51.37683",
+              "longitude": "-0.09728"
+           },
+           
+           "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
+           "telephone": "+44 0808 175 1749",
+           "aggregateRating": {
+                 "@type": "AggregateRating",
+                 "ratingValue": "",
+                 "ratingCount": ""
+               },
+          "datePublished": ""
+         }
+document.write(new Date().getFullYear());
+Value for custom_js_code
