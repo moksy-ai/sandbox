30a2ce1043c1f8e1b8249054a4dea236aac5eff6
mr-tajul-Islam <tajul.azazil@gmail.com>
2026-02-16 18:33:30 +0600

    ok

---
+++ templates/bootcamp/statica/ai/assets/scripts/scripts.js
@@ -0,0 +1,39 @@
+{
+           "@context": "http://www.schema.org",
+           "@type": "ProfessionalService",
+           "name": "Statica",
+           "title": "Statica - Quality Online Learning for All Ages",
+           "url": "/",
+           "sameAs": [
+              ""
+           ],
+           "logo": "https://moksy.com/api/build/uploads/82878324/uploads/logo.png",
+           "priceRange": "$$$",
+           "image": "https://moksy.com/api/build/uploads/82878324/uploads/socialshare.png",
+           "description": "Statica offers quality online learning for all ages. Explore our diverse range of courses and start your educational journey today.",
+           "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "3-5, Marco Polo House, Lansdowne Road",
+              "addressLocality": "London",
+              "addressRegion": "Surrey",
+              "postalCode": "CR0 2BX",
+              "addressCountry": "United Kingdom"
+           },
+           "email": "support@moksy.com",
+           "geo": {
+              "@type": "GeoCoordinates",
+              "latitude": "51.37683",
+              "longitude": "-0.09728"
+           },
+           
+           "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
+           "telephone": "+44 0808 175 1749",
+           "aggregateRating": {
+                 "@type": "AggregateRating",
+                 "ratingValue": "",
+                 "ratingCount": ""
+               },
+          "datePublished": ""
+         }
+document.write(new Date().getFullYear());
+Value for custom_js_code
