c06dcdcde60e798950e3c8f973cc6a2bd5b26d68
mr-tajul-Islam <tajul.azazil@gmail.com>
2026-02-23 13:00:26 +0600

    changes

---
+++ templates/bootcamp/codas/ai/assets/scripts/scripts.js
@@ -0,0 +1,51 @@
+{
+           "@context": "http://www.schema.org",
+           "@type": "ProfessionalService",
+           "name": "Codas",
+           "title": "Codas - Cutting-Edge eLearning Solutions",
+           "url": "/",
+           "sameAs": [
+              ""
+           ],
+           "logo": "https://moksy.com/api/build/uploads/44606554/uploads/logo.png",
+           "priceRange": "$$$",
+           "image": "https://moksy.com/api/build/uploads/44606554/uploads/socialshare.png",
+           "description": "Discover cutting-edge eLearning solutions at Codas. Our innovative courses and resources are designed for modern learners.",
+           "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "3-5, Marco Polo House, Lansdowne Road",
+              "addressLocality": "London",
+              "addressRegion": "Surrey",
+              "postalCode": "CR0 2BX",
+              "addressCountry": "United Kingdom"
+           },
+           "email": "support@moksy.com",
+           "geo": {
+              "@type": "GeoCoordinates",
+              "latitude": "51.37683",
+              "longitude": "-0.09728"
+           },
+           
+           "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
+           "telephone": "+44 0808 175 1749",
+           "aggregateRating": {
+                 "@type": "AggregateRating",
+                 "ratingValue": "",
+                 "ratingCount": ""
+               },
+          "datePublished": ""
+         }
+var currentDate = new Date(); var day = currentDate.getDate(); var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; var month = monthNames[currentDate.getMonth()]; var year = currentDate.getFullYear(); document.write(month + ' ' + day + ', ' + year);
+var currentDate = new Date(); var day = currentDate.getDate(); var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; var month = monthNames[currentDate.getMonth()]; var year = currentDate.getFullYear(); document.write(month + ' ' + day + ', ' + year);
+var currentDate = new Date(); var day = currentDate.getDate(); var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; var month = monthNames[currentDate.getMonth()]; var year = currentDate.getFullYear(); document.write(month + ' ' + day + ', ' + year);
+$(".pricing-table-col").on("mouseover", function () {
+      var animation = 'swing';  // Use 'swing' animation from Animate.css
+      
+      $(".pricing-table-col").removeClass("highlight");
+      $(".pricing-table-col .action").removeClass("animate__animated animate__" + animation);
+      
+      $(this).addClass("highlight");
+      $(this).find(".action").addClass("animate__animated animate__" + animation);
+    });
+document.write(new Date().getFullYear());
+Value for custom_js_code
