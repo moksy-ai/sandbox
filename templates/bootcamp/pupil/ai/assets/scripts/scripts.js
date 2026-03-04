854d05fd0e52af994d778b0967f4187e95fe197f
mr-tajul-Islam <tajul.azazil@gmail.com>
2026-03-04 15:33:08 +0600

    changes

---
+++ templates/bootcamp/pupil/ai/assets/scripts/scripts.js
@@ -0,0 +1,39 @@
+{
+           "@context": "http://www.schema.org",
+           "@type": "ProfessionalService",
+           "name": "Lumen",
+           "title": "Lumen - Online Learning for Professional Excellence",
+           "url": "/",
+           "sameAs": [
+              ""
+           ],
+           "logo": "https://moksy.com/api/build/uploads/49663731/uploads/logo.png",
+           "priceRange": "$$$",
+           "image": "https://moksy.com/api/build/uploads/49663731/uploads/socialshare.png",
+           "description": "Achieve professional excellence with online learning from Lumen. Explore our courses and start your journey today.",
+           "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "3-5, Marco Polo House, Lansdowne Road",
+              "addressLocality": "London",
+              "addressRegion": "Surrey",
+              "postalCode": "CR0 2BX",
+              "addressCountry": "United Kingdom"
+           },
+           "email": "support@moksy.com",
+           "geo": {
+              "@type": "GeoCoordinates",
+              "latitude": "51.37683",
+              "longitude": "-0.09728"
+           },
+           
+           "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
+           "telephone": "+44 0808 175 1749",
+           "aggregateRating": {
+                 "@type": "AggregateRating",
+                 "ratingValue": "",
+                 "ratingCount": ""
+               },
+          "datePublished": ""
+         }
+document.write(new Date().getFullYear());
+Value for custom_js_code
