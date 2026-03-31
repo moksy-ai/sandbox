13ec63238f00961a8b3291744864fd6eec439aea
mr-tajul-Islam <tajul.azazil@gmail.com>
2026-03-31 13:06:44 +0600

    changes

---
+++ templates/bootcamp/dataly/ai/assets/scripts/scripts.js
@@ -0,0 +1,45 @@
+{
+           "@context": "http://www.schema.org",
+           "@type": "ProfessionalService",
+           "name": "Dataly",
+           "title": "Dataly - Flexible Learning for Busy Schedules",
+           "url": "/",
+           "sameAs": [
+              ""
+           ],
+           "logo": "https://moksy.com/api/build/uploads/84470415/uploads/logo.png",
+           "priceRange": "$$$",
+           "image": "https://moksy.com/api/build/uploads/84470415/uploads/socialshare.png",
+           "description": "Dataly offers flexible learning for busy schedules. Explore our courses and start learning at your own pace today.",
+           "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "3-5, Marco Polo House, Lansdowne Road",
+              "addressLocality": "London",
+              "addressRegion": "Surrey",
+              "postalCode": "CR0 2BX",
+              "addressCountry": "United Kingdom"
+           },
+           "email": "support@moksy.com",
+           "geo": {
+              "@type": "GeoCoordinates",
+              "latitude": "51.37683",
+              "longitude": "-0.09728"
+           },
+           
+           "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
+           "telephone": "+44 0808 175 1749",
+           "aggregateRating": {
+                 "@type": "AggregateRating",
+                 "ratingValue": "",
+                 "ratingCount": ""
+               },
+          "datePublished": ""
+         }
+var currentDate = new Date(); var day = currentDate.getDate(); var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; var month = monthNames[currentDate.getMonth()]; var year = currentDate.getFullYear(); document.write(month + ' ' + day + ', ' + year);
+var currentDate = new Date(); var day = currentDate.getDate(); var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; var month = monthNames[currentDate.getMonth()]; var year = currentDate.getFullYear(); document.write(month + ' ' + day + ', ' + year);
+var currentDate = new Date(); var day = currentDate.getDate(); var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; var month = monthNames[currentDate.getMonth()]; var year = currentDate.getFullYear(); document.write(month + ' ' + day + ', ' + year);
+var currentDate = new Date(); var day = currentDate.getDate(); var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; var month = monthNames[currentDate.getMonth()]; var year = currentDate.getFullYear(); document.write(month + ' ' + day + ', ' + year);
+var currentDate = new Date(); var day = currentDate.getDate(); var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; var month = monthNames[currentDate.getMonth()]; var year = currentDate.getFullYear(); document.write(month + ' ' + day + ', ' + year);
+var currentDate = new Date(); var day = currentDate.getDate(); var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; var month = monthNames[currentDate.getMonth()]; var year = currentDate.getFullYear(); document.write(month + ' ' + day + ', ' + year);
+document.write(new Date().getFullYear());
+Value for custom_js_code
