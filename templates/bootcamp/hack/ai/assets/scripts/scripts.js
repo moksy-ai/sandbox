f56a4b0e21baebae3ed73f59f4447069d3202c4b
mr-tajul-Islam <tajul.azazil@gmail.com>
2026-04-23 19:33:09 +0600

    changes

--- templates/bootcamp/hack/ai/assets/scripts/scripts.js
+++ templates/bootcamp/hack/ai/assets/scripts/scripts.js
@@ -61,3 +61,12 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+const progressBar = document.getElementById("progress-bar");
+
+window.addEventListener("scroll", () => {
+  const scroll = window.scrollY;
+  const height = document.documentElement.scrollHeight - window.innerHeight;
+  const progress = (scroll / height) * 100;
+
+  progressBar.style.height = progress + "%";
+});
